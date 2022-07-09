9465b2c81477ae8b8cd53a01bdd37e97a5cdc5bf
dmitriy.rashevskiy <dmitriy.rashevskiy@vizor-games.com>
2022-07-10 00:56:37 +0300

    Создана ClientTabPanel c компонентами ClientListPanel и ClientPanel

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,20 @@
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.google.inject.Guice
 import entity.Client
-import ui.client.ClientPanel
-import ui.clientList.ClientListPanel
+import ui.ClientTabPanel
 
 class Main{
 
 }
 
 val selectedClient: MutableState<Client?> = mutableStateOf(null)
-val inj = Guice.createInjector()
-val clientPanel = inj.getInstance(ClientPanel::class.java).apply { init(selectedClient) }
-val clientListPanel = inj.getInstance(ClientListPanel::class.java).apply { init(selectedClient) }
+val clientTabPanel = Guice.createInjector().getInstance(ClientTabPanel::class.java).apply { init() }
 
 fun main() = application {
     Window(
@@ -30,19 +24,7 @@ fun main() = application {
         resizable = false
     ) {
         MaterialTheme {
-            Row {
-                Column(modifier = Modifier.fillMaxHeight().fillMaxWidth(0.3f)) {
-                    clientListPanel.show()
-                }
-
-                Column {
-                    Divider(color = MaterialTheme.colors.primary, modifier = Modifier.fillMaxHeight().width(1.dp))
-                }
-
-                Column(modifier = Modifier.fillMaxHeight().fillMaxWidth()) {
-                    clientPanel.show()
-                }
-            }
+            clientTabPanel.show()
         }
     }
 }
