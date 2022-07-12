3eaf7f481db673e178a6ba302007acd4e07a25ba
vladislav.kolyada <vladislav.kolyada@vizor-games.com>
2022-07-12 21:47:19 +0300

    добавлена сборка джарки

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,17 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
     id("org.jetbrains.kotlin.plugin.jpa") version "1.6.10"
     id("org.jetbrains.compose") version "1.1.0"
+    id ("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 repositories {
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("https://plugins.gradle.org/m2/")
     google()
 }
 
@@ -24,6 +27,7 @@ dependencies {
 
     implementation("javax.inject:javax.inject:1")
     implementation("com.google.inject:guice:4.2.3")
+    implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
 }
 
 val compileKotlin: KotlinCompile by tasks
@@ -34,5 +38,18 @@ compileKotlin.kotlinOptions {
 compose.desktop {
     application {
         mainClass = "MainKt"
+    }
+}
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        exclude("**/bd**")
+        exclude("**/hibernate.properties")
+        append("**/hibernate.build.properties")
+        rename("hibernate.build.properties", "hibernate.properties")
+        archiveFileName.set("FitnessApp.jar")
+        manifest {
+            attributes(mapOf("Main-Class" to "MainKt"))
+        }
     }
 }
